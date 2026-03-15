31e6779d4a10a78183b927f947f13365e2f933c9
drzzln <drzzln@protonmail.com>
2026-03-15 19:56:19 -0400

    add nix flake with substrate integration

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "OpenShift Helm chart repository with certified charts for Red Hat OpenShift";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    substrate = { url = "github:pleme-io/substrate"; inputs.nixpkgs.follows = "nixpkgs"; };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, substrate, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [ kubernetes-helm kubectl ];
+      };
+    });
+}
