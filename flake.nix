9dc33b8ab372f1b48840dd1874910e09c927d42a
drzzln <drzzln@protonmail.com>
2026-03-15 20:55:06 -0400

    fix flake.nix structure and add flake.lock

--- flake.nix
+++ flake.nix
@@ -1,7 +1,11 @@
 {
   description = "OpenShift Helm chart repository with certified charts for Red Hat OpenShift";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs { inherit system; };
