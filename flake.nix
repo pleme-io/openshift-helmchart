fd9ad09a1c55cdf1be78eb94f05a45d1e9bddf46
drzzln <drzzln@protonmail.com>
2026-03-15 20:35:26 -0400

    cleanup: remove stale comments, fake hashes, unused inputs, wire overlay

--- flake.nix
+++ flake.nix
@@ -2,10 +2,7 @@
   description = "OpenShift Helm chart repository with certified charts for Red Hat OpenShift";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    substrate = { url = "github:pleme-io/substrate"; inputs.nixpkgs.follows = "nixpkgs"; };
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-  outputs = { self, nixpkgs, substrate, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs { inherit system; };
     in {
